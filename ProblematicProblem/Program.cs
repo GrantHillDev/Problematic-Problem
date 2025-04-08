d6eb4f02999b1261fd17929272a20045b57e043a
Grant Hill <granthill@live.com>
2025-04-07 22:58:39 -0400

    Updated TC Problematic Problem assignment -- still working on it, but it runs fine, until the very end.

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -4,17 +4,17 @@ using System.Threading;
 
 namespace ProblematicProblem
 {
-    public static class Program
+    public static class Program//all compile errors have been fixed, but there's still runtime errors that need to be tested and fixed as well.
     {
         static Random rng;
-        static bool cont = true;
+        static string cont;//static bool cont = true;
         static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
 
         static void Main(string[] args)
         {
 
-            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-            cont = bool.Parse(Console.ReadLine());
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");//Console.Write is a method used to continuously keep all printed data contained and written on the same line, whereas Console.WriteLine will print data to different lines each time it is called (in other words, you could call Console.Write multiple times, and as long as they're all called within the same block of code, each typed out instance will be combined into one line).
+            cont = Console.ReadLine();//cont = bool.Parse(Console.ReadLine());
             Console.WriteLine();
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
@@ -23,8 +23,8 @@ namespace ProblematicProblem
             int userAge = int.Parse(Console.ReadLine());//TryParse didn't work here for some reason.
             Console.WriteLine();
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
-            if (seeList)
+            var seeList = Console.ReadLine();//bool seeList = bool.Parse(Console.ReadLine());
+            if (seeList == "yes" || seeList == "Yes" || seeList == "Sure" || seeList == "sure")
             {
                 foreach (string activity in activities)
                 {
@@ -33,11 +33,16 @@ namespace ProblematicProblem
                 }
                 Console.WriteLine();
             }
+            else if(seeList == "no" || seeList == "No")
+            {
+                Console.WriteLine("Not to worry, carry on then.");
+                Console.WriteLine();
+            }
             Console.Write("Would you like to add any activities before we generate one? yes/no: ");
 
-            bool addToList = bool.Parse(Console.ReadLine());
+            var addToList = Console.ReadLine();//bool addToList = bool.Parse(Console.ReadLine());
             Console.WriteLine();
-            while (addToList)
+            while (addToList == "yes" || seeList == "Yes")
             {
                 Console.Write("What would you like to add? ");
                 string userAddition = Console.ReadLine();
@@ -49,10 +54,10 @@ namespace ProblematicProblem
                 }
                 Console.WriteLine();
                 Console.WriteLine("Would you like to add more? yes/no: ");
-                addToList = bool.Parse(Console.ReadLine());//addToList has already had its value declared above.
+                addToList = Console.ReadLine();//addToList = bool.Parse(Console.ReadLine());//addToList has already had its value declared above. Needed to have tis type changed any per its own logic and runtime error.
             }//strongly typed; once a variable has been delcared, its type cannot be changed.
 
-            while (cont)
+            while (cont == "yes" || cont == "Yes")
             {
                 Console.Write("Connecting to the database");
                 for (int i = 0; i < 10; i++)
@@ -70,7 +75,7 @@ namespace ProblematicProblem
                 Console.WriteLine();
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+                if (userAge < 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
@@ -78,9 +83,9 @@ namespace ProblematicProblem
                     randomNumber = rng.Next(activities.Count);
                     randomActivity = activities[randomNumber];
                 }
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                 Console.WriteLine();
-                bool cont = bool.Parse(Console.ReadLine());
+                cont = Console.ReadLine();//bool cont = bool.Parse(Console.ReadLine());
             }
         }
     }
